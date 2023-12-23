d39a4017dd68ea0f2abd530b224bb3b7552adce0
Achmad Fatkharrofiqi <achmadfatkharrofiqi404@gmail.com>
2023-12-24 01:47:19 +0700

    feat: add https

--- usecases/property_usecase.go
+++ usecases/property_usecase.go
@@ -64,7 +64,7 @@ func (p *PropertyUsecase) Delete(id int64) error {
 func processPhotoURLs(properties []models.Property, minio *helpers.MinioStorage) {
 	for i := range properties {
 		for j := range properties[i].PhotoHouse {
-			properties[i].PhotoHouse[j].PhotoUrl = constants.MinioEndpoint + "/" + constants.BucketName + "/" + properties[i].PhotoHouse[j].PhotoUrl
+			properties[i].PhotoHouse[j].PhotoUrl = "https://" + constants.MinioEndpoint + "/" + constants.BucketName + "/" + properties[i].PhotoHouse[j].PhotoUrl
 		}
 	}
 }
